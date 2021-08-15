20d90ca140a961514bf2e35f456d0b7fface4b91
taufiqhidy <46922081+taufiqhidy@users.noreply.github.com>
2021-08-15 12:12:38 +0700

    Create sadapsadapan2D.py

---
+++ sadapsadapan2D.py
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+#Tools SadapWhatsApp
+#Sadap WhatsApp
+
+clear
+
+sleep 1
+logo
+if [ xMilzuu = 4 ] || [ xMilzuu = 04 ]
+then
+\033[1;97m╭──[Silahkan Masukan Nomernya]>"
+read -p"  ╰───────root@MILZU-TC=▪> Milzu;
+sleep 2
+print ("""\033[1;97m [+]Scaning....
+sleep 8
+\033[1;97m10%
+sleep 7
+\033[1;97m20%
+sleep 5
+\033[1;97m30%
+sleep 4
+\033[1;97m40%
+sleep 5.5
+\033[1;97m50%
+sleep 4
+\033[1;97m60%
+sleep 5
+\033[1;97m70%
+sleep 6
+\033[1;97m80%
+sleep 7
+\033[1;97m90%
+sleep 5
+\033[1;97m100%
+sleep 3
+"Prosessing Done"
+sleep 4
+\033[1;97m "Nomer WhatsApp Berhasil Di Sadap "Verifikasi"fi
+""")
+
+if [ xMilzu = 0 ] || [ xMilzu = 00 ]
+then
+       "Anjim"
+       "Njer"
+       "Shutdown"
+exit
+fi
